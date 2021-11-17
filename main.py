adbb430b92e861baf9ddcb1a595d9075cf3d2093
matthadd <matthis.haddouche@gmail.com>
2021-11-17 03:51:20 +0100

    comments

--- main.py
+++ main.py
@@ -5,22 +5,36 @@ from eth_utils import decode_hex
 
 
 def bruteforce(false_private_key):
+    # this function build a dictionnary that contains all the possible pk and the associate address
+
+    # this dict will contain all of that
     allPossibility = {}
+
+    # this list contain all of the 0-9A-Za-z char (as int)
     allCharsCode = [i for i in range(ord('A'), ord('Z') + 1)] + [i for i in range(ord('0'), ord('9') + 1)] + [i for i in range(ord('a'),ord('z') + 1)]
 
     for j in range(len(false_private_key)):
+        #for each char of the pk we re gonna to
+
         for i in allCharsCode:
+            #circle through all the possibilites
+
+            # some junk to modify the char i of a string
             pk = list(false_private_key)
             pk[j] = chr(i)
             pk = ''.join(pk)
             allPossibility[pk] = None
 
+    # for each pk that derives from the original false pk we get the address (wich is not the pb, but derives from it)
     for pk in allPossibility.keys():
         try:
             allPossibility[pk] = getAddressFromPrivateKey2(pk)
+
+        # sometimes a pk does not have a pb so we ignore it (long story short the pk is in char, not in bytes so sometimes there is no pb associate)
         except Exception:
             pass
 
+    # print this dict (to check how many pk have a pb, if it does not render then you have more than 1 char wrong...)
     for pk in allPossibility.keys():
         if allPossibility[pk]:
             print(pk, allPossibility[pk])
@@ -28,6 +42,7 @@ def bruteforce(false_private_key):
 
 
 def getAddressFromPrivateKey(private_key):
+    # get the address of a pk
     private_key = "0x" + private_key
     acct = Account.from_key(private_key)
     return "0x" + str(acct.address)
